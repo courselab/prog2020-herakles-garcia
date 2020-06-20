78b863c5177e0f914a5ba1a048cda7f7ca205e48
herakles-garcia <62970739+herakles-garcia@users.noreply.github.com>
2020-06-20 14:00:21 -0300

    Update m002.c

--- m002.c
+++ m002.c
@@ -27,8 +27,69 @@
 
 void sort (int *a, int *b, int *c)
 {
-}
 
+int x;
+int y;
+int z;
+
+   x = *a;
+   y = *b;
+   z = *b;
+   
+if (x<y && x<z)
+{
+   *a = x;           //se x for o menor dos três, ocupará a posição a
+      if (y<z)
+      {
+         *b = y;     //se y for o segundo menor, ocupará a posição b
+         *c = z;     //se z for o maior, ocupará a posição c
+      }
+      else
+      {
+         *b = z;     //caso contrário, y é o maior, então y ocupará a posição c
+         *c = y;
+      }
+}
+         
+else
+{
+   if (y<x && y<z)   
+   {
+      *a = y;        //se y for o menor dos três, ocupará a posição a
+      
+      if (x<z)      
+      {
+         *b = x;     //se x for o segundo menor, ocupará a posição b
+         *c = z;     //se z for o maior, ocupará a posição c
+      }
+      
+      else
+      {
+         *b = z;    //caso contrário, x é o maior, então x ocupará a posição c
+         *c = x;
+      }
+   }
+   
+   else
+  
+   {
+      *a = z;      //se z for o menor dos três, ocupará a posição a
+      
+      if(x<y)
+      {
+         *b = x;   //se x for o segundo menor, ocupará a posição b
+         *c = y;   //se y for o maior, ocupará a posição c
+      }
+      
+      else
+      {
+         *b = y;  
+         *c = x;  //caso contrário, x é o maior, então x ocupará a posição c
+      }
+   }
+}   
+}
+   
 /* Do not edit this function. */
 
 int main (int argc, char **argv)
