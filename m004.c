cdd931ef3cdaa91eb80af44fb15a71ab0f75dbcf
herakles-garcia <62970739+herakles-garcia@users.noreply.github.com>
2020-06-21 21:00:31 -0300

    Update m004.c

--- m004.c
+++ m004.c
@@ -32,27 +32,17 @@ int decimal (char *b)
   int i;
   int n;
   int count = 0;
-  int base=1;
+  int base = 1;
   n = strlen (b);
   
-  for (i = 0; i <= n - 1 ; i++)
+  for (i = n-1; i >= 0 ; i--)
   {
-     if (b[i] == '1')
+     if (b[i]=='1')
      {
         count = count + base;
-        base = base * 2;
      }
      
-     if (b[i] == '0')
-     {
-         base = base * 2;
-     }
-     
-     if (b[i] != 0 || b[i] != 1)
-     {
-         break;
-     }
-     
+     base = base * 2;
   }
   return count;
 }
